93498d8a4af4165f1ca02abf2bd76167ed01c59f
Aurore Rigault <rigault.aurore@gmail.com>
2022-06-14 20:19:35 +0200

    first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Tier.destroy_all
+Character.destroy_all
+
+######   TIER    #####
+
+tier_x = Tier.new(name: 'X')
+tier_x.save!
+tier_o = Tier.new(name: 'O')
+tier_o.save!
+tier_s = Tier.new(name: 'S')
+tier_s.save!
+tier_a = Tier.new(name: 'A')
+tier_a.save!
+tier_b = Tier.new(name: 'B')
+tier_b.save!
+tier_c = Tier.new(name: 'C')
+tier_c.save!
