a0f39ff941bf34c36e4f847b18b18b51a7730a08
Aurore Rigault <rigault.aurore@gmail.com>
2022-06-15 15:39:51 +0200

    tier added to characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,6 @@
 class Character < ApplicationRecord
-  belongs_to :tier
   has_one :image
 
-  validates :name, :specialty, :origin, presence: true
+  validates :name, :specialty, :origin, :tier, presence: true
   validates :name, uniqueness: true
 end
